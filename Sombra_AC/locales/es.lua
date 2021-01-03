947bfbf81c8c00c9e5aad6bbf13a443af47db9a5
Stay <36738990+Quique1222@users.noreply.github.com>
2021-01-03 22:38:28 +0100

    Add files via upload

---
+++ Sombra_AC/locales/es.lua
@@ -0,0 +1,7 @@
+Locales['es'] = {
+    ['welcome'] = '~p~Bienvenido~w~ a ~g~SB-AC',
+    ['welcome2'] = '~g~SB-AC ~p~Menu ~w~de ~y~admin',
+    ['opadmin'] = '~y~> ~w~Opciones de ~p~administrador',
+    ['onplayers'] = '~p~> ~w~Jugadores ~y~conectados',
+    ['srvertool'] = '~y~> ~w~Herramientas de ~p~servidor',
+}
